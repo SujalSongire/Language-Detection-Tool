0214c84a0ac6ebfcfc48b61d0ef8f6454b9c2dcb
Sujal Songire <songiresujal@gmail.com>
2024-09-21 10:36:43 +0530

    Updated

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 with open('model.pckl', 'rb') as model_file:
     Lrdetect_Model = pickle.load(model_file)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     text_input = request.form['text_input']
     prediction = Lrdetect_Model.predict([text_input])
     return render_template('index.html', language=prediction[0])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
