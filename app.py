cc9cb7746f6767f2b892d36d2664f4b06eb9c218
Sujal Songire <songiresujal@gmail.com>
2025-01-28 19:56:04 +0530

    Update

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
+import os
 
-app = Flask(__name__)
+# Ensure the template folder is correctly set
+TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
 
+# Load the model
 with open('model.pckl', 'rb') as model_file:
     Lrdetect_Model = pickle.load(model_file)
 
@@ -12,9 +16,9 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    text_input = request.form['text_input']
+    text_input = request.form.get('text_input', '')
     prediction = Lrdetect_Model.predict([text_input])
     return render_template('index.html', language=prediction[0])
 
